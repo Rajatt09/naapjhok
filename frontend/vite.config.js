2ee4ec05eb1dd92352b7a349bfadcefea8d88f79
Rajatt09 <rajatbhati9559@gmail.com>
2026-01-27 00:26:42 +0530

    removed proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,14 +5,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173,
-     proxy: {
-      "/api": {
-        target: import.meta.env.VITE_BACKEND_URL,
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
 })
